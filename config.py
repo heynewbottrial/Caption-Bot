aa564a963d786941f8e089e1f52091f91d5bc668
electricalscammer <155905329+electricalscammer@users.noreply.github.com>
2024-02-14 23:11:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import os
 
 class Config(object):
     # env vars
-    BOT_TOKEN = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # string
-    API_ID = 1234567  # int
-    API_HASH = "123abc456def789ghi012jklmn"  # string
+    BOT_TOKEN = "6260622606:AAFPTnZx_zy1OKbwwt4bIcrspwbyPsIi8Pk"  # string
+    API_ID = 24262622  # int
+    API_HASH = "50831eb3329ed9c0557aa2bc6aa34376"  # string
     
     # db vars
     # keep empty if don't want to add any extra caption
-    CAPTION_TEXT = "Added this caption"  
+    CAPTION_TEXT = "Download By:- Electric Hacker"  
     # BOTTOM or TOP or NIL
     CAPTION_POSITION = "BOTTOM"  
-    ADMIN_USERNAME = "username"  # without "@". 
+    ADMIN_USERNAME = "ElectricHacker"  # without "@". 
 
     # a list of strings of words to remove from the existing caption
-    WORDS_TO_REMOVE = []  
+    WORDS_TO_REMOVE = [r."Download. .ExtracedBy"]  
     # a list of regex pattern strings to remove from the existing caption. 
     # For eg. r".*Join.*" will remove the entire line having word Join
     REGEX_PATTERNS = []  
